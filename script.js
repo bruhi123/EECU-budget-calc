023fee17070c7e3ec9410db0f624431511876d82
DevinKC261 <devin26chanthirath@gmail.com>
2026-03-06 14:23:46 -0800

    Simple JS setup

---
+++ script.js
@@ -0,0 +1,20 @@
+//-- Main mouthly income//
+const salarySelect = document.getElementById('salary');
+const investSelect = document.getElementById('investments');
+const businessSelect = document.getElementById('business');
+const OtherSelect = document.getElementById('other');
+
+//-- Main budget //
+const houseSelect = document.getElementById('housing');
+const utilSelect = document.getElementById('utilities');
+const groceSelect = document.getElementById('groceries');
+const transSelect = document.getElementById('transportation');
+const shopSelect = document.getElementById('shopping');
+const entertainSelect = document.getElementById('entertainment');
+const subSelect = document.getElementById('subscriptions');
+const healthSelect = document.getElementById('health');
+const internetSelect = document.getElementById('internet');
+const eduSelect = document.getElementById('education');
+const saveSelect = document.getElementById('saving');
+const travelSelect = document.getElementById('travel');
+
